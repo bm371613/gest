c0adb30e45a9003c31e39c23e6ce8e984dbb6d3b
Bartosz Marcinkowski <b.marcinkowski@leomail.pl>
2020-09-22 22:29:29 +0200

    introducing y-pinch gesture to model training

--- gest/model/training.py
+++ gest/model/training.py
@@ -7,12 +7,20 @@ from torch.nn import functional as F
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from torchvision.datasets import ImageFolder
 
-from gest.model import IMAGE_WIDTH, IMAGE_HEIGHT
+from gest.model import CLASS_COUNT, IMAGE_WIDTH, IMAGE_HEIGHT
+from gest.model.dataset import Dataset
 
 
-class BinaryClassifier(pl.LightningModule):
+def vectorize_target(target):
+    class_index, additional_annotations = target
+    return torch.Tensor([
+        class_index,
+        0 if additional_annotations is None else additional_annotations
+    ])
+
+
+class Model(pl.LightningModule):
 
     def __init__(self):
         super().__init__()
@@ -27,17 +35,32 @@ class BinaryClassifier(pl.LightningModule):
             nn.Flatten(),
             nn.Linear(64 * (IMAGE_HEIGHT // 2 ** 3) * (IMAGE_WIDTH // 2 ** 3), 64), nn.ReLU(),
             nn.Dropout(),
-            nn.Linear(64, 1), nn.Sigmoid(),
-            nn.Flatten(0),
+            nn.Linear(64, 4), nn.Sigmoid(),
         )
 
     def forward(self, x):
         return self.net(x)
 
-    def training_step(self, batch, batch_idx):
+    def loss(self, batch):
         x, y = batch
-        loss = F.binary_cross_entropy(self(x), y.float())
-        return pl.TrainResult(loss)
+        class_indexes = y[:, 0].long()
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat[:, :CLASS_COUNT], class_indexes)
+        mask = class_indexes == 2
+        loss += F.mse_loss(y_hat[mask, CLASS_COUNT:], y[mask, 1:])
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        loss = self.loss(batch)
+        result = pl.TrainResult(loss)
+        result.log('train_loss', loss)
+        return result
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.loss(batch)
+        result = pl.EvalResult(checkpoint_on=loss)
+        result.log('val_loss', loss)
+        return result
 
     def configure_optimizers(self):
         return Adam(self.parameters())
@@ -50,7 +73,7 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", help="Number of epochs", type=int, default=1)
     args = parser.parse_args()
 
-    dataset = ImageFolder(
+    dataset = Dataset(
         args.data_path,
         transform=transforms.Compose([
             transforms.RandomHorizontalFlip(),
@@ -58,8 +81,9 @@ if __name__ == "__main__":
             transforms.Resize((IMAGE_HEIGHT, IMAGE_WIDTH)),
             transforms.ToTensor(),
         ]),
+        target_transform=vectorize_target,
     )
-    model = BinaryClassifier()
+    model = Model()
     trainer = pl.Trainer(logger=False, max_epochs=args.epochs)
     trainer.fit(model, DataLoader(dataset, 32, shuffle=True))
     torch.onnx.export(
