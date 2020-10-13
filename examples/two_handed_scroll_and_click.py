5d0512b8e3eab3c361d33068200a91290f0db6e1
Bartosz Marcinkowski <b.marcinkowski@leomail.pl>
2020-10-14 01:07:19 +0200

    two handed scrolling: separate scrolling thread

--- examples/two_handed_scroll_and_click.py
+++ examples/two_handed_scroll_and_click.py
@@ -1,4 +1,5 @@
 import argparse
+import threading
 import time
 
 import cv2
@@ -6,7 +7,7 @@ import pynput.mouse
 
 from gest.cv_gui import text, draw_inferred_crossheads, show_inference_result
 from gest.inference import InferenceSession
-from gest.math import relative_average_coordinate, accumulate
+from gest.math import relative_average_coordinate
 
 parser = argparse.ArgumentParser()
 parser.add_argument("model_file", help="Model file")
@@ -23,22 +24,35 @@ class App:
 
         self.scrolling_sensitivity = scrolling_sensitivity
         self.score_threshold = .5
+        self.scrolling_speed = 0
+
+    def scroll_forever(self):
+        last_time = time.time()
+        distance = 0
+        while True:
+            time.sleep(0.1)
+            now = time.time()
+            distance += (now - last_time) * self.scrolling_speed * self.scrolling_sensitivity
+            self.mouse.scroll(0, int(distance))
+            distance -= int(distance)
+            last_time = now
 
     def run(self):
+        scrolling_thread = threading.Thread(target=self.scroll_forever)
+        scrolling_thread.daemon = True
+        scrolling_thread.start()
+
         capture = cv2.VideoCapture(self.camera)
         capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
         capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
         button_down = None
         button_down_since = None
-        scroll_acc = 0
-        scroll_distance = 0
-        last_time = None
         last_click = None
         while True:
             ret, frame = capture.read()
             if not ret:
                 break
-            this_time = time.time()
+            now = time.time()
 
             # inference
             inference_result = self.inference_session.cv2_run(frame)
@@ -60,26 +74,23 @@ class App:
                 else:
                     button_down_now = 'right click'
             elif abs(left_y - right_y) > .1:
-                scroll_now = right_y - left_y
+                scroll_now = left_y - right_y
+
             if button_down != button_down_now:
-                button_down_since = this_time
-                if button_down_now == 'click' and (last_click is None or this_time - last_click > .5):
+                button_down_since = now
+                if button_down_now == 'click' and (last_click is None or now - last_click > .5):
                     self.mouse.click(pynput.mouse.Button.left)
-                    last_click = this_time
-                if button_down_now == 'double click' and (last_click is None or this_time - last_click > .5):
+                    last_click = now
+                if button_down_now == 'double click' and (last_click is None or now - last_click > .5):
                     self.mouse.click(pynput.mouse.Button.left, 2)
-                    last_click = this_time
+                    last_click = now
                 button_down = button_down_now
-            elif button_down_now == 'right click' and .5 < this_time - button_down_since and \
+            elif button_down_now == 'right click' and .5 < now - button_down_since and \
                     (last_click is None or last_click < button_down_since):
                 self.mouse.click(pynput.mouse.Button.right)
-                last_click = this_time
+                last_click = now
 
-            scroll_acc = accumulate(scroll_acc, scroll_now)
-            if last_time:
-                scroll_distance += scroll_acc * (last_time - this_time) * self.scrolling_sensitivity
-                self.mouse.scroll(0, int(scroll_distance))
-                scroll_distance -= int(scroll_distance)
+            self.scrolling_speed = scroll_now
 
             frame = draw_inferred_crossheads(frame, inference_result)
             cv2.imshow('Camera', text(cv2.flip(frame, 1), "Press ESC to quit"))
@@ -87,7 +98,6 @@ class App:
 
             if cv2.waitKey(1) & 0xFF == 27:  # esc to quit
                 break
-            last_time = this_time
 
         capture.release()
         cv2.destroyAllWindows()
